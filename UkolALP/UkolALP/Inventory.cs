897cbc7e9b4d5c14d3fca769e4c54354ecb5576d
Foxiar <75813110+Foxiar@users.noreply.github.com>
2023-05-05 12:11:59 +0200

    Add files via upload

---
+++ UkolALP/UkolALP/Inventory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UkolALP
+{
+    internal class Inventory
+    {
+        public List<Product> Products = new List<Product>();
+
+
+        public int count { get; set; }
+
+        public void AddProduct(string name, double price) {
+            try
+            {
+                Product checkNameProduct = Products.Find(x => x.Name == name);
+                if (checkNameProduct != null) {
+                    Console.WriteLine("Položka už nesmí být v inventáři!");
+                }
+                else
+                {
+                    Products.Add(new Product { Name = name, Price = price });
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Nastala neočekávaná chyba! Jsi si jistý, že jsi dosadil do prvního argumentu řetězec znaků a do druhého číslo?");
+            }
+        }
+        public void RemoveProduct(string name) {
+            Product smazanyProdukt = Products.Find(x => x.Name == name);
+            if (smazanyProdukt != null)
+            {
+                Products.Remove(smazanyProdukt);
+            }
+            else
+            {
+                Console.WriteLine("Tato položka není v inventáři!");
+            }
+        }
+    }
+}
---
+++ UkolALP/UkolALP/Program.cs
@@ -0,0 +1,68 @@
+﻿using UkolALP;
+
+Inventory inventar = new Inventory();
+string nazevPolozky;
+double cenaPolozky;
+
+while (true)
+{
+    Console.WriteLine("1 - Přidat novou položku");
+    Console.WriteLine("2 - Mazat existující položku");
+    Console.WriteLine("-----------------------");
+    Console.WriteLine("Seznam položek:");
+    foreach (var item in inventar.Products)
+    {
+        Console.WriteLine($"{item.Name} {item.Price}");
+    }
+    try
+    {
+        byte vyber = Convert.ToByte(Console.ReadLine());
+        switch (vyber)
+        {
+            case 1:
+                Console.Clear();
+                Console.WriteLine("Napiš název položky:");
+                nazevPolozky = Console.ReadLine();
+                Console.WriteLine("Napiš cenu položky:");
+                try
+                {
+                    cenaPolozky = Convert.ToDouble(Console.ReadLine());
+                    if (cenaPolozky < 0)
+                    {
+                        Console.WriteLine("Cena položky nemůže mít zápornou hodnotu!");
+                    }
+                    else
+                    {
+                        inventar.AddProduct(nazevPolozky, cenaPolozky);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Cena položky musí být číslo!");
+                }
+                break;
+            case 2:
+                Console.Clear();
+                Console.WriteLine("Napiš název položky, kterou chceš smazat.");
+                string nazevPolozkyDelete = Console.ReadLine();
+                inventar.RemoveProduct(nazevPolozkyDelete);
+
+                Console.WriteLine("Seznam položek:");
+                foreach (var item in inventar.Products)
+                {
+                    Console.WriteLine($"{item.Name} {item.Price}");
+                }
+
+                break;
+
+            default:
+                break;
+        }
+    }
+    catch (FormatException)
+    {
+        Console.Clear();
+        Console.WriteLine("Nastala chyba! Musíš zadat číslo!");
+    }
+}
